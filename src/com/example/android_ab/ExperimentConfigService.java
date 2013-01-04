25afc5a805fecfde7a0e31320d7ea440c1e96bb4
Erik Eldridge <erik@twitter.com>
2013-01-03 17:36:46 -0800

    Use BuildConfig.DEBUG in service

--- src/com/example/android_ab/ExperimentConfigService.java
+++ src/com/example/android_ab/ExperimentConfigService.java
@@ -2,8 +2,6 @@ package com.example.android_ab;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import org.json.JSONArray;
 
@@ -11,12 +9,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ExperimentConfigService extends IntentService {
     private static final String TAG = "ExperimentConfigService";
-    private static final String SERVICE_URL = "http://damp-taiga-5766.localhost.com:5000";
+    private static final String SERVICE_URL = "http://damp-taiga-5766.herokuapp.com";
 
     public ExperimentConfigService() {
         super("ExperimentConfigService");
@@ -34,9 +33,14 @@ public class ExperimentConfigService extends IntentService {
         String json = "[]";
         try {
             URL url = new URL(SERVICE_URL);
+            if(BuildConfig.DEBUG){
+                url = new URL(SERVICE_URL+":5000");
+            }
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             json = readStream(con.getInputStream());
-        } catch(Exception e){
+        } catch(ConnectException e){
+            e.printStackTrace();
+        } catch(IOException e){
             e.printStackTrace();
         }
         return json;
@@ -51,6 +55,10 @@ public class ExperimentConfigService extends IntentService {
             reader = new BufferedReader(new InputStreamReader(in));
             while((line = reader.readLine()) != null){
                 json += line;
+                // http://stackoverflow.com/a/5190876
+                if (!reader.ready()) {
+                    break;
+                }
             }
         }catch(IOException e){
             e.printStackTrace();
@@ -91,5 +99,6 @@ public class ExperimentConfigService extends IntentService {
         DBHelper helper = new DBHelper(this);
         helper.removeAllExperiments();
         helper.addExperiments(experiments);
+        helper.close();
     }
 }
