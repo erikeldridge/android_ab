d4e50e822c0f928d2f147894f7983293549a6ef3
Erik Eldridge <erik@twitter.com>
2013-01-05 17:45:36 -0800

    refactor service integration to use message queue

--- src/com/example/android_ab/Experiment.java
+++ src/com/example/android_ab/Experiment.java
@@ -1,6 +1,42 @@
 package com.example.android_ab;
 
-public class Experiment {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Experiment implements Parcelable{
     public String mExperimentKey, mBucketName, mExpirationDate;
     public int mVersion;
+
+    public Experiment() {
+    }
+
+    public Experiment(Parcel in) {
+        mExperimentKey = in.readString();
+        mBucketName = in.readString();
+        mVersion = in.readInt();
+        mExpirationDate = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel in, int i) {
+        in.writeString(mExperimentKey);
+        in.writeString(mBucketName);
+        in.writeInt(mVersion);
+        in.writeString(mExpirationDate);
+    }
+
+    public static final Parcelable.Creator<Experiment> CREATOR = new Parcelable.Creator<Experiment>() {
+        public Experiment createFromParcel(Parcel in) {
+            return new Experiment(in);
+        }
+
+        public Experiment[] newArray(int size) {
+            return new Experiment[size];
+        }
+    };
 }
--- src/com/example/android_ab/ExperimentConfigService.java
+++ src/com/example/android_ab/ExperimentConfigService.java
@@ -2,37 +2,41 @@ package com.example.android_ab;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.util.Log;
-import com.fasterxml.jackson.core.JsonFactory;
-import org.json.JSONArray;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
-
 public class ExperimentConfigService extends IntentService {
-    private static final String TAG = "ExperimentConfigService";
-    private static final String SERVICE_URL = "http://damp-taiga-5766.herokuapp.com";
+    private static final String DEV_SERVICE_URL = "http://damp-taiga-5766.herokuapp.com:5000";
+    private static final String PROD_SERVICE_URL = "http://damp-taiga-5766.herokuapp.com";
+    private static final String EXTRA_EXPERIMENTS = "experiments";
+
+    final Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            final Intent intent = (Intent) msg.obj;
+            final ArrayList<Experiment> experiments = intent.getParcelableArrayListExtra(EXTRA_EXPERIMENTS);
+            saveExperiments(experiments);
+        }
+    };
 
     public ExperimentConfigService() {
         super("ExperimentConfigService");
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
-        Log.v(TAG, "onHandleIntent");
+    protected void onHandleIntent(final Intent intent) {
         try {
             InputStream stream = fetchExperimentConfig();
             ArrayList<Experiment> experiments = Parser.parseExperimentArray(stream);
-            saveExperiments(experiments);
+            intent.putParcelableArrayListExtra(EXTRA_EXPERIMENTS, experiments);
+            mHandler.sendMessage(mHandler.obtainMessage(1, intent));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,10 +44,10 @@ public class ExperimentConfigService extends IntentService {
 
     private InputStream fetchExperimentConfig() throws IOException {
         URL url;
-        if(BuildConfig.DEBUG){
-            url = new URL(SERVICE_URL+":5000");
-        }else{
-            url = new URL(SERVICE_URL);
+        if(BuildConfig.DEBUG) {
+            url = new URL(DEV_SERVICE_URL);
+        } else {
+            url = new URL(PROD_SERVICE_URL);
         }
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         return con.getInputStream();
